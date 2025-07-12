f6c5c264ad2cfea90259b25d3e3d7b1b90b6d9ab
Hernan Hawryluk <hernanhawryluk@gmail.com>
2025-07-12 15:35:57 -0300

    Add filters for operating system and skills

--- htbmachines.sh
+++ htbmachines.sh
@@ -30,7 +30,10 @@ function helpPanel() {
   echo -e "\t${purpleColour}u)${endColour} ${grayColour}Descargar o actualizar archivos necesarios.${endColour}"
   echo -e "\t${purpleColour}m)${endColour} ${grayColour}Buscar por un nombre de máquina.${endColour}"
   echo -e "\t${purpleColour}i)${endColour} ${grayColour}Buscar por dirección IP.${endColour}"
-  echo -e "\t${purpleColour}i)${endColour} ${grayColour}Obtener link de la resolucón de la máquina en YouTube.${endColour}"
+  echo -e "\t${purpleColour}d)${endColour} ${grayColour}Buscar por la dificultad de una máquina.${endColour}"
+  echo -e "\t${purpleColour}o)${endColour} ${grayColour}Buscar por el sistema operativo.${endColour}"
+  echo -e "\t${purpleColour}s)${endColour} ${grayColour}Buscar por skill.${endColour}"
+  echo -e "\t${purpleColour}y)${endColour} ${grayColour}Obtener link de la resolucón de la máquina en YouTube.${endColour}"
   echo -e "\t${purpleColour}h)${endColour} ${grayColour}Mostrar este panel de ayuda.${endColour}\n"
 }
 
@@ -44,7 +47,7 @@ function updateFiles() {
     echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Todos los archivos han sido descargados.${endColour}"
   else
     echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Comprobando si hay actualizaciones pendientes...${grayColour}\n"
-    sleep 2
+    sleep 1
     curl -s $main_url > bundle_temp.js
     js-beautify bundle_temp.js | sponge bundle_temp.js
     md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
@@ -55,7 +58,7 @@ function updateFiles() {
       rm bundle_temp.js
     else 
       echo -e "${yellowColour}[+]${endColour} ${grayColour}Se han encontrado actualizaciones disponibles.${endColour}\n"
-      sleep 2
+      sleep 1
       rm bundle.js && mv bundle_temp.js bundle.js  
       echo -e "${yellowColour}[+]${endColour} ${grayColour}Los archivos han sido actualizados.${endColour}"
     fi
@@ -101,15 +104,82 @@ function getYouTubeLink() {
   fi
 }
 
+function getMachinesDifficulty() {
+  difficulty="$1"
+
+  results_check="$(cat bundle.js | grep "dificultad: \"$difficulty\"" -i -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '""' | tr -d ',' | column)"
+
+  if [ "$results_check" ]; then
+    echo -e "\n${yellowColour}[+]${endCoulour} ${grayColour}Representando las máquinas que poseen un nivel de dificultad${endColour} ${blueColour}${difficulty}${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "dificultad: \"$difficulty\"" -i -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '""' | tr -d ',' | column
+  else 
+    echo -e "\n${redColour}[!]${endColour} ${grayColour}La dificultad indicada no existe.${endColour}"
+
+    echo -e "\n${blueColour}[+]${endColour} ${grayColour}Dificultades permitidas para la búsqueda:${endColour}"
+    echo -e "\n${blueColour}[+]${endColour} ${grayColour}Fácil - Media - Difícil - Insane${endColour}\n"
+  fi
+}
+
+function getOSMachines() {
+  os="$1"
+
+  os_results="$(cat bundle.js | grep "so: \"$os\"" -i -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
+
+  if [ "$os_results" ]; then
+    echo -e "${yellowColour}[+]${endColour} ${grayColour}Mostrando las máquinas cuyo sistema operativo es${endColour} ${blueColour}$os${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "so: \"$os\"" -i -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!]${endColour} ${grayColour}El sistema operativo indicado no existe.${endColour}"
+    echo -e "\n${blueColour}[+]${endColour} ${grayColour}Sistemas operativos permitidos para la búsqueda:${endColour}"
+    echo -e "\n${blueColour}[+]${endColour} ${grayColour}Linux - Windows${endColour}\n"
+  fi
+}
+
+function getOSDifficultyMachines() {
+  difficulty="$1"
+  os="$2"
+
+  check_results="$(cat bundle.js | grep "so: \"$os\"" -i -B 5 -A 1 | grep "dificultad: \"$difficulty\"" -i -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')"
+
+  if [ "$check_results" ]; then
+    echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Se va a aplicar una búsqueda por la${endColour} ${blueColour}$difficulty${endColour} ${grayColour}y tengan sistema operativo${endColour} ${blueColour}$os${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "so: \"$os\"" -B 5 -A 1 | grep "dificultad: \"$difficulty\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!]${endColour} ${grayColour}No hay resultados para la búsqueda.${endColour}"
+    echo -e "\n${blueColour}[+]${endColour} ${grayColour}Parametros aceptados para sistema operativo: -o [\"Linux\", \"Windows\"].${endColour}"
+    echo -e "\n${blueColour}[+]${endColour} ${grayColour}Parametros aceptados para dificultad: -d [\"Facíl\", \"Medio\", \"Difícil\", \"Insane\"].${endColour}"
+  fi
+}
+
+function getSkill() {
+  skill="$1"
+
+  skill_checker="$(cat bundle.js | grep "skills: " -B 6 | grep "$skill" -i -B 6 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
+
+  if [ "$skill_checker" ]; then
+    echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Se representan las máquinas donde se ven la técnicas${endColour} ${blueColour}$skill${endColour}${grayColour}:${endColour}\n"
+    cat bundle.js | grep "skills: " -B 6 | grep "$skill" -i -B 6 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+  else
+    echo -e "\n${redColour}[!]${endColour} ${grayColour}No se a encontrado ninguna máquina con la skill indicada.${endColour}\n" 
+  fi
+}
+
 # Indicadores
 declare -i parameter_counter=0
 
-while getopts "m:ui:y:h" arg; do
+# Chivatos
+declare -i chivato_difficulty=0
+declare -i chivato_os=0
+
+while getopts "m:ui:y:d:o:s:h" arg; do
   case $arg in
     m) machineName="$OPTARG"; let parameter_counter+=1;;
     u) let parameter_counter+=2;;
     i) ipAddress="$OPTARG"; let parameter_counter+=3;;
     y) machineName="$OPTARG"; let parameter_counter+=4;;
+    d) difficulty="$OPTARG"; chivato_difficulty=1; let parameter_counter+=5;;
+    o) os="$OPTARG"; chivato_os=1; let parameter_counter+=6;;
+    s) skill="$OPTARG"; let parameter_counter+=7;;
     h) ;;
   esac
 done
@@ -122,6 +192,14 @@ elif [ $parameter_counter -eq 3 ]; then
   searchIP $ipAddress
 elif [ $parameter_counter -eq 4 ]; then
   getYouTubeLink $machineName
+elif [ $parameter_counter -eq 5 ]; then
+  getMachinesDifficulty $difficulty
+elif [ $parameter_counter -eq 6 ]; then
+  getOSMachines $os
+elif [ $parameter_counter -eq 7 ]; then
+  getSkill "$skill"
+elif [ $chivato_difficulty -eq 1 ] && [ $chivato_os -eq 1 ]; then
+  getOSDifficultyMachines $difficulty $os
 else
   helpPanel
 fi
